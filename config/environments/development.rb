a0cc19d0131aeb1250f7e3c56618728bd5a00fac
carmenhledesma <carmenh.ledesma@gmail.com>
2016-05-04 09:31:08 +0200

    login with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Set up the default URL options for the Devise mailer in each environment
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   mount Ckeditor::Engine => '/ckeditor'
   resources :posts
+  root to: 'posts#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
